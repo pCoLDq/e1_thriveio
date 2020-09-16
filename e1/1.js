e6bbb4961db5e44e3ff330517f452db950fb13b8
pCoLDq <elvisisaev@gmail.com>
2020-09-16 14:20:10 +0300

    Complete all tasks except 8th

---
+++ e1/1.js
@@ -0,0 +1,20 @@
+// Определите переменные str, num, flag и txt со значениями «Привет», 123, true, «true».
+// При помощи оператора определения типа убедитесь, что переменных принадлежат типам: string, number, boolean.
+
+let str = 'Привет';
+let num = 123;
+let flag = true;
+let txt = 'true';
+
+if (typeof str === 'string') {
+  console.log("The type of 'str' variable is string");
+}
+if (typeof num === 'number') {
+  console.log("The type of 'num' variable is number");
+}
+if (typeof flag === 'boolean') {
+  console.log("The type of 'flag' variable is boolean");
+}
+if (typeof txt === 'string') {
+  console.log("The type of 'txt' variable is string");
+}
