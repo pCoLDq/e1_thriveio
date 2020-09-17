bb1c2ac15b0e8e51baa063e9ae1a4c507a5c0f53
pCoLDq <elvisisaev@gmail.com>
2020-09-17 09:22:08 +0300

    e1 test complete

--- e1/8.js
+++ e1/8.js
@@ -1,7 +1,18 @@
 // Даны размер ипотечного кредита (S — 2 млн.руб), процентная ставка (p  — 10%),
 // кол-во лет (years — 5). Найти переплату по кредиту, значение переплаты должно содержаться в переменной perepl.
 
-let S = 2; // millions rubles
+let S = 2000000; // rubles
 let p = 10; // percent
 let years = 5;
-let perepl;
+let perepl = 0;
+
+let St = S / (years * 12); // сумма, которая идёт на погашение тела кредита
+
+while (S) {
+  if (St > S) {
+    St = S;
+  }
+  perepl += (S * (p / 100)) / 12;
+  S -= St;
+}
+console.log(Math.round(perepl));
