fe144dac73e3fa69721358e6807e4970a8d6d3be
pCoLDq <elvisisaev@gmail.com>
2020-11-11 01:35:04 +0300

    Auth task completed

--- survey_express/index.js
+++ survey_express/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mysql = require('mysql2');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const crypto = require('crypto');
+const { triggerAsyncId } = require('async_hooks');
 
 const app = express();
 
@@ -7,8 +11,15 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   database: 'survey_express',
-  password: 'password',
+  password: 'ghj555666',
 });
+
+const authRouter = express.Router();
+const getHashedPassword = (password) => {
+  const sha256 = crypto.createHash('sha256');
+  const hash = sha256.update(password).digest('base64');
+  return hash;
+};
 
 connection.connect(function (err) {
   if (err) {
@@ -21,14 +32,115 @@ connection.connect(function (err) {
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/static');
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.post('/register', function (request, response) {
+  const { username, password, confPassword } = request.body;
+  if (password === confPassword) {
+    connection.query('SELECT * FROM users', (errUsers, resultsUsers, fieldsUsers) => {
+      if (resultsUsers.find((user) => user.username === username)) {
+        response.render('register.hbs', {
+          message: 'Username already registered',
+        });
+        return;
+      }
+    });
+
+    const hashedPassword = getHashedPassword(password);
+
+    connection.query(
+      "INSERT INTO `users` (`username`, `password`) VALUES ('" + username + "', '" + hashedPassword + "')",
+      (errUsers, resultsUsers, fieldsUsers) => {
+        console.log('user registered');
+      }
+    );
+
+    response.render('login.hbs', {
+      message: 'Registration Complete. Please login to continue.',
+    });
+  } else {
+    response.render('register.hbs', {
+      message: 'Passwords does not match.',
+    });
+  }
+});
+app.post('/login', function (request, response) {
+  const { username, password } = request.body;
+  const hashedPassword = getHashedPassword(password);
+  connection.query('SELECT * FROM users', (errUsers, resultsUsers, fieldsUsers) => {
+    // console.log(72, resultsUsers);
+    if (
+      resultsUsers.find((user) => {
+        if (user.username === username && user.password === hashedPassword) {
+          // console.log(76, user.id);
+          const generateAuthToken = () => {
+            return crypto.randomBytes(30).toString('hex');
+          };
+          const authToken = generateAuthToken();
+          response.cookie('AuthToken', authToken);
+
+          const qry = "INSERT INTO `authTokens` (`token`, `user_id`) VALUES ('" + authToken + "', " + user.id + ')';
+          // console.log(99, qry);
+          connection.query(qry, () => {
+            console.log('token created');
+          });
+          response.redirect('/');
+          return true;
+        } else {
+          return false;
+        }
+      })
+    ) {
+    } else {
+      response.render('login.hbs', {
+        message: 'Invalid username or password',
+      });
+      return;
+    }
+  });
+});
+
+app.get('/register', function (request, response) {
+  response.render('register.hbs');
+});
+app.get('/login', function (request, response) {
+  response.render('login.hbs');
+});
+
 app.get('/', function (request, response) {
+  let isAuthenticated = false;
+  let user = {};
+  connection.query('SELECT * FROM authTokens', function (errTokens, resultsTokens, fieldsTokens) {
+    // console.log(118, resultsTokens);
+    if (
+      resultsTokens.find((token) => {
+        if (token.token === request.cookies['AuthToken']) {
+          console.log(122, token);
+          isAuthenticated = true;
+          connection.query(
+            'SELECT * FROM users WHERE id IN (' + token.user_id + ')',
+            (errUsers, resultsUsers, fieldsUsers) => {
+              console.log(131, resultsUsers);
+              user = resultsUsers[0];
+            }
+          );
+          return true;
+        }
+      })
+    ) {
+    }
+  });
   connection.query('SELECT * FROM questions', function (errQuestions, resultsQuestions, fieldsQuestions) {
     connection.query(`SELECT * FROM answers WHERE question IN (${resultsQuestions[0]['id']})`, function (
       errAnswers,
       resultsAnswers,
       fieldsAnswers
     ) {
+      // console.log(143, request.cookies['AuthToken'], user.username);
       response.render('survey.hbs', {
+        isAuthenticated,
+        username: user.username,
         question: resultsQuestions[0]['text'],
         answer1: resultsAnswers[0]['text'],
         answer2: resultsAnswers[1]['text'],
@@ -42,15 +154,14 @@ app.get('/', function (request, response) {
     });
   });
 });
+
 app.get('/submit', function (request, response) {
-  console.log(request.query);
   connection.query('UPDATE `answers` SET `choices` = `choices` + 1 WHERE id = ' + request.query.answer, function (
     errAnswers,
     resultsAnswers,
     fieldsAnswers
   ) {
     connection.query('SELECT choices FROM answers WHERE question IN (1)', function (err, results, fields) {
-      console.log(results[1]);
       response.render('results.hbs', {
         resAnswer1: results[0]['choices'],
         resAnswer2: results[1]['choices'],
--- survey_express/survey_express.sql
+++ survey_express/survey_express.sql
@@ -39,8 +39,34 @@ CREATE TABLE `answers` (
 
 LOCK TABLES `answers` WRITE;
 /*!40000 ALTER TABLE `answers` DISABLE KEYS */;
-INSERT INTO `answers` VALUES (1,1,0,'Answer 1'),(2,1,5,'Answer 2'),(3,1,4,'Answer 3'),(4,1,1,'Answer 4');
+INSERT INTO `answers` VALUES (1,1,0,'Answer 1'),(2,1,5,'Answer 2'),(3,1,4,'Answer 3'),(4,1,5,'Answer 4');
 /*!40000 ALTER TABLE `answers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `authtokens`
+--
+
+DROP TABLE IF EXISTS `authtokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `authtokens` (
+  `token` varchar(100) NOT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`token`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `authtokens_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `authtokens`
+--
+
+LOCK TABLES `authtokens` WRITE;
+/*!40000 ALTER TABLE `authtokens` DISABLE KEYS */;
+INSERT INTO `authtokens` VALUES ('af40366b69d83231dbcca61336fd39535fe3d7f37064d24b0c22fb68192c',5),('e38ef3c987e3d46fc12e089d279b2334af973c4e8bf1881f1ab500bcbcfc',5),('ea2cd8f25a16b8737484c52aa6b2ef0f7e9804a3bee108143c90e838c9df',5),('fcfeb0809ea666bd79d5c8ac3d3fcdc11245604c17764d8c8cbbe0a48ac3',5);
+/*!40000 ALTER TABLE `authtokens` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -66,6 +92,31 @@ LOCK TABLES `questions` WRITE;
 INSERT INTO `questions` VALUES (1,'What kind of questions would you like to see here?');
 /*!40000 ALTER TABLE `questions` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'David','ghjghjghj'),(2,'Josh','zxczxczxc'),(3,'Josh','zxczxczxc'),(4,'isaev_en20','dcfX4qTJ5vPMs/xVDtOP9Cbp81pDxSIkl1mmOUIYmwY='),(5,'CoLD','Vslo8Topv3w62yzlZKWWyvl3yQALfiR5TucpzObsc9g=');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -76,4 +127,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-10-28 20:06:25
+-- Dump completed on 2020-11-11  1:34:10
